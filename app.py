e6c4ff8e40e207bddecb56aa169ecb2ae52709d3
William Hernandez <willhgco@gmail.com>
2024-04-29 17:20:44 -0500

    fix: normal loading flet setting

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ from main import Main
 
  
 def main():
-    ft.app(target=Main, view = ft.WEB_BROWSER)
-    #ft.app(target=Main)
+    #ft.app(target=Main, view = ft.WEB_BROWSER)
+    ft.app(target=Main)
     
 if __name__ == "__main__":
     main()
